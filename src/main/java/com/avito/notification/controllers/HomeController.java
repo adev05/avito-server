84cc51165b68a4c517d2d6abf1be8647de8aecea
Sergey <xramtsoff2005@yandex.ru>
2024-04-23 14:40:56 +0300

    Server fix

--- src/main/java/com/avito/notification/controllers/HomeController.java
+++ src/main/java/com/avito/notification/controllers/HomeController.java
@@ -2,13 +2,19 @@ package com.avito.notification.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@CrossOrigin(origins = "localhost:3000")
 @RestController
+@RequestMapping("/api/v1/")
 public class HomeController {
 
     @GetMapping("/")
     public String helloWorld() {
         return "Hello, World!";
     }
+
+    
 
 }
